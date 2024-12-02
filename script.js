a2d20fb2ad8a79b7a1382db9ad39849d0b7f14f9
Nahutilus <nahumartinou@gmail.com>
2024-12-01 22:38:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             // Quitar la clase 'selected' de todos los elementos
             listItems.forEach(li => li.classList.remove('selected'));
+            
             // Agregar la clase 'selected' al elemento clicado
             item.classList.add('selected');
+            
+            // Añadir animación de resplandor temporal
+            item.classList.add('highlight');
+            setTimeout(() => {
+                item.classList.remove('highlight');
+            }, 500); // Duración del efecto (en milisegundos)
         });
     });
 });
